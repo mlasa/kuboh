f9abb02060a63f315261d5a866f57af768801ae6
Marcella LASA <marcella.amorimsa@gmail.com>
2021-01-03 22:52:53 -0300

    validating price format

--- backend/src/services/CreateSpotService.ts
+++ backend/src/services/CreateSpotService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 
 import AppError from '../errors/AppError';
 import Spot from '../models/Spot';
+import User from '../models/User';
 
 interface Request {
   owner_id: string;
@@ -29,6 +30,12 @@ class CreateSpotService {
     price,
     description,
   }: Request): Promise<Spot> {
+
+    const userRepository = getRepository(User);
+    const ownerExists = await userRepository.findOne({ where: { id: owner_id } });
+    if (!ownerExists)
+      throw new AppError('Owner not found, owner_id not valid');
+
     const spotRepository = getRepository(Spot);
 
     if (!state || !city || !complement || !street || !neighborhood || !cep) {
@@ -38,12 +45,20 @@ class CreateSpotService {
         400,
       );
     }
-    if (!price) throw new AppError('Price is a needed information', 400);
+
+    if (!price) throw new AppError('Price is a required information', 400);
+
+
+    const regexMoneyFormat = /^\d{1,}(\.\d{2})?$/;
+    console.log('valor convertido pra string', price.toString())
+    if (!regexMoneyFormat.test(price.toString())) {
+      console.log('resultado do teste', regexMoneyFormat.test(price.toString()))
+      throw new AppError('Price format invalid, valid shapes: 0 ou 0.00');
+    }
 
     const spot = spotRepository.create({
       owner_id,
-      localization: `${street},${neighborhood},${number},${city},${state},${cep},
-      ${complement}`,
+      localization: `${street},${neighborhood},${number},${city},${state},${cep},${complement}`,
       price,
       description,
     });
